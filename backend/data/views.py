8c63c637824260e8bd30437e1fa93ad8176bd22d
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-03-11 15:56:10 -0400

    PUT PATCH DELETE for individual list items working

--- backend/data/views.py
+++ backend/data/views.py
@@ -20,5 +20,24 @@ def list_items(request, format=None):
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-      
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def list_items_detail(request, id, format=None):
+  
+  try:
+    list_item = ListItem.objects.get(pk=id)
+  except ListItem.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    serializer = ListItemSerializer(list_item)
+    return Response(serializer.data)
+  elif request.method == 'PUT':
+    serializer = ListItemSerializer(list_item, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  elif request.method == 'DELETE':
+    list_item.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
