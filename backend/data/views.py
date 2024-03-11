8d740a34811a0c6d949ba1dd571f687b2cc5bd40
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-03-11 15:30:42 -0400

    POSTMAN can now POST new list items

--- backend/data/views.py
+++ backend/data/views.py
@@ -3,9 +3,20 @@
 from django.http import JsonResponse
 from .models import ListItem
 from .serializers import ListItemSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def list_items(request, format=None):
   
+  if request.method == 'GET':
     ListItems = ListItem.objects.all()
     serializer = ListItemSerializer(ListItems, many=True)
     return JsonResponse({'list_items' : serializer.data}, headers={'Access-Control-Allow-Origin': '*'})
+
+  if request.method == 'POST':
+    serializer = ListItemSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
