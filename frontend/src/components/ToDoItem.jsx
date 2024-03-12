a1078fa7b693c83dce5b58b63ba35cdc7807a5c2
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-03-12 15:08:21 -0400

    completed status change button now works

--- frontend/src/components/ToDoItem.jsx
+++ frontend/src/components/ToDoItem.jsx
@@ -18,10 +18,29 @@ export default function ToDoItem({ name, completed, id, setState }) {
     });
   }
 
+  const handleStatusChange = (id) => {
+    
+    setDone(!done);
+
+    axios.put(`http://localhost:8000/list_items/${id}`, {
+    name: name,  
+    completed: !done
+    }).then((response) => {
+      console.log(response);
+    })
+    .then(() => {
+      axios.get(
+        "http://localhost:8000/list_items.json").then((response) => {
+        console.log(response);
+        setState({ list: response.data });
+      });
+    });
+  }
+
   return (
     <div>
       <p>{ name }</p>
-      <button onClick={() => setDone(!done)}>{done ? "Done" : "Not Yet"}</button>
+      <button onClick={() => handleStatusChange(id)}>{done ? "Done" : "Not Yet"}</button>
       <button onClick={() => handleDelete(id)}>Delete</button>
     </div>
   );
