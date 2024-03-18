bc8a526c58b9539acc61f39574c33b03e58127f6
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-03-18 16:45:41 -0400

    verified token works

--- backend/data/views.py
+++ backend/data/views.py
@@ -1,12 +1,14 @@
 # Endpoints
-
 from .models import ListItem
 from .serializers import ListItemSerializer, UserSerializer
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET', 'POST'])
 def list_items(request, format=None):
@@ -45,7 +47,12 @@ def list_items_detail(request, id, format=None):
   
 @api_view(['POST'])
 def login(request, format=None):
-  return Response({})
+  user = get_object_or_404(User, username=request.data['username'])
+  if not user.check_password(request.data['password']):
+    return Response({"detail": "Not found"}, status=status.HTTP_401_UNAUTHORIZED)
+  token, created = Token.objects.get_or_create(user=user)
+  serializer = UserSerializer(instance=user)
+  return Response({'token': token.key, 'user': serializer.data})
 
 @api_view(['POST'])
 def signup(request, format=None):
@@ -59,6 +66,13 @@ def signup(request, format=None):
     return Response({'token': token.key, 'user': serializer.data})
   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def test_token(request, format=None):
-  return Response({})
+  return Response({"passed for {}".format(request.user.username)})
