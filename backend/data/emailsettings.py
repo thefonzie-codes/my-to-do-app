7f2e92de99a600cf93a9a3b74900a5c46839cf75
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-03-19 18:08:16 -0400

    fixed security issue on git

---
+++ backend/data/emailsettings.py
@@ -0,0 +1,7 @@
+## Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'alfonsobanzon@gmail.com'
+EMAIL_HOST_PASSWORD = 'pgzs yuht ovpm eygr' 
