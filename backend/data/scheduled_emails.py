7a0b72e939acd557e433f123d3978ff125dd0dca
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-04-09 16:58:30 -0400

    sends a check in email now as well

--- backend/data/scheduled_emails.py
+++ backend/data/scheduled_emails.py
@@ -12,11 +12,29 @@ def send_daily_reminder():
     ListItems = ListItem.objects.filter(due_date=date.today())
     serializer = ListItemSerializer(ListItems, many=True)
     data = serializer.data
-    html_message = render_to_string("email-templates.html", {'tasks': data})
+    html_message = render_to_string("reminder.html", {'tasks': data})
     plain_message = strip_tags(html_message)
     subject='Your To-Do List for Today'
     from_email='alfonsobanzon@gmail.com'
     to=['al_banzon@hotmail.com']
+
+    send_mail(
+      subject, 
+      plain_message, 
+      from_email, 
+      to, 
+      html_message=html_message,
+      fail_silently=False)
+    
+def send_daily_checkin():
+    ListItems = ListItem.objects.filter(due_date=date.today())
+    serializer = ListItemSerializer(ListItems, many=True)
+    data = serializer.data
+    html_message = render_to_string("check-in-email.html", {'tasks': data})
+    plain_message = strip_tags(html_message)
+    subject='Did you complete your tasks today?'
+    from_email='alfonsobanzon@gmail.com'
+    to=['al_banzon@hotmail.com', 'anderson.ashley@live.ca']
 
     send_mail(
       subject, 
