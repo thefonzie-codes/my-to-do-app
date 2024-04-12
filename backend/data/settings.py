eb00ac7a4113d79b3712aedbebc96beda2e4caeb
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-04-12 14:46:19 -0400

    front and backend work in docker

--- backend/data/settings.py
+++ backend/data/settings.py
@@ -78,8 +78,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'data.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
-  'http://localhost:5173',
-  'http://127.0.0.1:5173',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
 ]
 
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    host: true,
+    port: 3000,
   }
 })
