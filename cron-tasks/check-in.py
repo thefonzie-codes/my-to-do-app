d326f72782285ae2e8277e93020f97ab2ec8a1e9
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-04-29 16:33:06 -0400

    simplifying cron jobs by separating them

---
+++ cron-tasks/check-in.py
@@ -0,0 +1,9 @@
+import requests
+
+def ping_endpoint():
+    url = 'my-to-do-app-production.up.railway.app/email_checkin/'
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}, Response: {response.text}")
+
+if __name__ == "__main__":
+    ping_endpoint()
