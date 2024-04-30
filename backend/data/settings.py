d0d6736f9fe9522ffb749313a1ed4ad091cc255e
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-04-30 16:14:19 -0400

    removing crontab from backend

--- backend/data/settings.py
+++ backend/data/settings.py
@@ -162,8 +162,3 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('APP_PASSWORD')
-
-CRONJOBS = [
-  ('*/5 * * * *', 'data.scheduled_emails.send_daily_reminder'),
-  ('*/5 * * * *', 'data.scheduled_emails.send_daily_checkin'),
-]
