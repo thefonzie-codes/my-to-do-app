52d6586383d44fa5ce12fb1bbe9340cb5618b467
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-05-04 17:40:46 -0400

    attempting form field overrides

--- backend/data/serializers.py
+++ backend/data/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import ListItem
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ListItemSerializer(serializers.ModelSerializer):
     user_id = serializers.PrimaryKeyRelatedField(source='user', queryset=User.objects.all())
