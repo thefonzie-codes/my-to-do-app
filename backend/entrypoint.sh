b7b3a2c49d7ca5d9bb1ff168bd6d0c0e079d3ab4
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-05-17 15:54:06 -0400

    removed staticfiles and made changes to run cron via docker

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,6 +11,12 @@ python manage.py migrate
 echo "Creating superuser..."
 python manage.py create_superuser
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "starting cron jobs"
+python manage.py crontab add
+
 # Start the main process
 echo "Starting server..."
 exec "$@"
