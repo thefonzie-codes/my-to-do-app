4b4020b86f1371fc23910fe200a8e9b9bfa63b2f
Alfonso Banzon <alfonsobanzon@gmail.com>
2024-05-17 14:26:49 -0400

    removed js version of frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/my-to-do-app/',
   plugins: [react()],
-  build: {
-    target: 'es2022',
-  },
   server: {
-    host: true,
-    port: 3000,
-  },
-  preview: {
-    host: true,
     port: 3000,
   }
 })
